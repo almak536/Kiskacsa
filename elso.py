5a5188c63a3c1898b7496cafbbedb00d2b4ddfd3
almak536 <almakompot536@gmail.com>
2023-09-14 16:24:00 +0200

    második szinkron

--- elso.py
+++ elso.py
@@ -1,1 +1,19 @@
-print("Üdv a fedélzeten")
+# print('Üdv \na\nfedélzeten', end=' *_* ')
+"""print('\nSzia', 'Jenő\n', sep='_')
+
+print('''Ez 
+több
+sorba
+kerül
+''')
+"""
+
+nev = input("Hogy hívnak: ")
+titulus = "dr."
+print('Szia', nev)
+print(f'Szia {nev}')
+print('Haliho {0}{1}'.format(titulus, nev))
+
+print('jobbra'.rjust(50,'_'))
+print('balra'.ljust(50,'*'))
+print('center'.center(50))
